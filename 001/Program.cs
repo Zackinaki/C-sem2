16017b44718252c474099a5a24802d02b5e983f7
IrinchinovZh <exp.bur@gmail.com>
2022-11-25 13:36:37 +0800

    change1

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// task demo
+Console.Clear();
+int n = new Random().Next(10, 100); // [10, 99]
+int n1 = n / 10;
+int n2 = n % 10;
+Console.WriteLine($"Случайное число: {n}");
+if (n1 > n2)
+    Console.Write(n1);
+else
+    Console.Write(n2);
