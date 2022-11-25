64ed89a3b273cf10988b80d8be2b5a6fc194bde5
IrinchinovZh <exp.bur@gmail.com>
2022-11-25 13:27:29 +0800

    change

---
+++ 002/Program.cs
@@ -0,0 +1,7 @@
+﻿// task 11
+Console.Clear();
+int n = new Random().Next(100, 1000); // [100, 999]
+Console.WriteLine($"Случайное число: {n}");
+int n1 = n / 100;
+int n3 = n % 10;
+Console.WriteLine(n1 * 10 + n3);
---
+++ 003/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.Write("Vvedite chislo pervoe: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Vvedite chislo vtoroe: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(b%a==0)
+    Console.Write("kratno");
+else
+{
+    Console.WriteLine("ne kratno");
+    int c =b%a;
+    Console.WriteLine(c);
+}
+
+// task 12
+// Console.Clear();
+
+// Console.Write("Введите 1-ое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// if (n % m == 0)
+//     Console.Write("Кратно");
+// else
+//     Console.Write($"Не кратно, остаток {n % m}");
+
+ 
